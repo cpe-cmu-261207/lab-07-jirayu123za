e90f3d351ce9e0c5e8dd55224d5a0d3e9c0149cf
jirayu123za <76035470+jirayu123za@users.noreply.github.com>
2021-09-05 20:52:12 +0700

    Complete?

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,3 +1,5 @@
+import { setColorPick, PixelPainterStore } from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
 }
@@ -5,14 +7,21 @@ type SelectColorButtonProps = {
 const SelectColorButton = ({ color }: SelectColorButtonProps) => {
 
   //modify this function to highlight correctly
+  const state = PixelPainterStore.useState()
+
   const computeRingSize = () => {
-    return ""
-    return "ring-8 ring-green-400"
+
+    if (color === state.colorPick)
+      return "ring-green-400 ring-8"
+    else
+      return ""
+    // return "hover:ring-green-400 hover:ring-8"
   }
 
   return (
-    <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
+    <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer `}
       style={{ backgroundColor: color }}
+      onClick={() => { setColorPick(color); console.log(color) }}
     >
     </div>
   )
--- src/components/Utility.tsx
+++ src/components/Utility.tsx
@@ -1,10 +1,12 @@
+import { clearPage, randomPage } from "../stores/PixelPainterStore"
+
 const Utility = () => {
   return (
     <div className="flex justify-center space-x-3">
-      <button className="w-36">Clear</button>
-      <button className="w-36">Random color</button>
+      <button className="w-36" onClick={clearPage}>Clear</button>
+      <button className="w-36" onClick={randomPage}>Random color</button>
     </div>
   )
 }
 
-export default Utility
+export default 
